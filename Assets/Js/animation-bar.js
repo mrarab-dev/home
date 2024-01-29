5642a6970138f26046ac2a801a7ac8b7c642fec8
mohammadreza <mohammad79reza.arab@gmail.com>
2024-01-29 23:34:26 +0330

    add project

---
+++ Assets/Js/animation-bar.js
@@ -0,0 +1,7 @@
+function startAnimation() {
+    document.querySelector('.animation-bar').style.width = '80%';
+  }
+  
+  function resetAnimation() {
+    document.querySelector('.animation-bar').style.width = '0';
+  }
